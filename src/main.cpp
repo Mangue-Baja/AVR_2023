59d9a7b9cde6647b36ddc078fbc5adc33b0e5b49
joaogabrieldonato <jgsd.2001@gmail.com>
2023-11-07 21:18:15 -0300

    ESP-NOW communication ok

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@
 #include "soft_defs.h"
 #include "hard_defs.h"
 
-#define DEBOUCE_TIME 150
 /* Defines type of ESP32 function */
 #define M_0
 //#define M_30
@@ -14,28 +13,21 @@
 
 /* Libraries Variables */
 LiquidCrystal_I2C lcd(0x27, 16, 2); 
+esp_now_peer_info_t peerInfo;
 
 /* Global Variables */
-bool sd_exist = false;
 bool esp_now_ok = false;
-bool B_SEL_CLICK = false, B_CANCEL_CLICK = false;
-uint8_t M_30_ok = 0, M_100_101_ok = 0;
+bool conf_30 = false, conf_100 = false;
+uint8_t AddressFor_0[] = { 0x40, 0x91, 0x51, 0xFB, 0xEA, 0x18 };
 
-/* ESP-NOW Callbacks */
-void receiveCallBack(const uint8_t* macAddr, const uint8_t* data, int len);
-void formatMacAddress(const uint8_t* macAddr, char* info, int maxLength); 
-void sentCallBack(const uint8_t* macAddr, esp_now_send_status_t status);
-/* Interrupts routine */
-#ifdef M_0
-  void select_button();
-  void cancel_button();
-#endif
-/* Global functions */
+/* Global Functions */
 void Pin_Config();
-bool sent_to_all(const String &msg);
-bool read_config();
-void Mount_SD();
-char potSelect(uint8_t pin, uint8_t num_options);
+/* ESP-NOW Functions */
+void receiveCallBack(const uint8_t* macAddr, const uint8_t* data, int len);
+void sentCallBack(const uint8_t* macAddr, esp_now_send_status_t status);
+void formatMacAddress(const uint8_t* MACAddr, char* info, int maxLength);
+bool sent_to_all(const uint8_t msg);
+bool sent_to_single(const uint8_t msg);
 
 void setup() 
 {
@@ -46,14 +38,22 @@ void setup()
 
   // Put ESP32 into Station Mode
   WiFi.mode(WIFI_MODE_STA);
-  Serial.printf("Mac address: %s\n", WiFi.macAddress());
+  Serial.printf("Mac address: ");
+  Serial.println(WiFi.macAddress());
+  /*
+    * E1_0 = 40:91:51:FB:EA:18
+    * E2_30 = 40:91:51:FC:EA:CC
+    * E3_100 = EC:62:60:9C:BD:DC
+  */
 
   // Disconnect from Wifi
   WiFi.disconnect();
 
-  lcd.init();
-  lcd.backlight();
-  lcd.clear();
+  #ifdef M_0
+    lcd.init();
+    lcd.backlight();
+    lcd.clear();
+  #endif
 }
 
 void loop() 
@@ -62,157 +62,79 @@ void loop()
   {
     case INIT:
     {
-      // Initialize ESP-NOW
       if(esp_now_init()==ESP_OK)
       {
-        //esp_now_ok = true;
-        Serial.println("Init ESP-NOW Protocol");
+        //Serial.println("Init ESP_NOW Protocol");
 
-        /* Broadcast Functions */
+        /* Broadcast Functions */ 
         esp_now_register_recv_cb(receiveCallBack);
         esp_now_register_send_cb(sentCallBack);
 
-      } else {
-        
-        #ifdef M_30
-          digitalWrite(LED_BUILTIN, HIGH);
-          esp_restart();
-        #elif defined(M_100_101)
-          digitalWrite(LED_BUILTIN, HIGH);
-          esp_restart();
+        #if defined(M_30) || defined(M_100_101)
+          // Register peer
+          memcpy(peerInfo.peer_addr, AddressFor_0, 6);
+          peerInfo.channel = 0;
+          peerInfo.encrypt = false;
+
+          // Add peer
+          if(esp_now_add_peer(&peerInfo)!=ESP_OK)
+          {
+            Serial.println("Failed to add peer");
+            return;
+          }
         #endif
 
-        Serial.println("ESP-NOW init Failed!!");
+      } else {
+
+        #if defined(M_30) || defined(M_100_101)
+          digitalWrite(LED_BUILTIN, HIGH);
+          delay(1500);
+          esp_restart();
+
+          break;
+        #endif  
+
+        //Serial.println("ESP-NOW init Failed!!");
         digitalWrite(LED_BUILTIN, HIGH);
 
         lcd.print("ESP-NOW ERROR");
-        delay(500);
+        delay(2000);
         lcd.clear();
         lcd.print("Reiniciando");
-        delay(500);
-        lcd.setCursor(0,13); lcd.write('.');
-        delay(1000);
-        lcd.setCursor(0,14); lcd.write('.');
-        delay(1000);
-        lcd.setCursor(0,15); lcd.write('.');
+        delay(1000); lcd.print('.');
+        delay(1000); lcd.print('.');
+        delay(1000); lcd.print('.');
 
         delay(500);
         esp_restart(); 
       }
 
-      #ifdef M_30
-        while(M_30_ok!=2) { Serial.println("Await Please"); }
-        
-        st = IDLE;
-        break;
+      #if defined(M_30) || defined(M_100_101)
+        st=WAIT;
+      #else
+        while(!esp_now_ok || !conf_30 || !conf_100)
+        {
+          //Serial.printf("%d %d %d", esp_now_ok, conf_30, conf_100);
+          sent_to_all(1);
+          delay(500);
+        }
 
-      #elif defined(M_100_101)
-      while(M_100_101_ok!=2) { Serial.println("Await please"); }
-      
-      st = IDLE
-      break;
-
+        st = MENU;
+        //(sent_to_all(1) ? st=WAIT : 0);
       #endif
-
-      bool ss = sent_to_all("ok");
-
-      ss ? st=WAIT : 0;
+      
+      break;
     }
-      break;
-
-    case IDLE:
-      delay(1);
-      break;
-
+      
     case WAIT:
-
-      if(esp_now_ok && listen_30 && listen_100_101)
-      {
-        lcd.clear();
-        lcd.print(F("Tudo Certo!"));
-        delay(500);
-        lcd.clear();
-
-        listen_30 = false;
-        listen_100_101 = false;
-
-        st = BEGIN;
-
-      } else {
-        //lcd.clear();
-        lcd.print(F("Testando..."));
-      }
-      
+    {
       break;
+    }
 
-    case BEGIN:
-
-      /*
-      #ifdef M_30
-        
-        st = ST_30;
-        break;
-      #elif defined(M_100_101)
-        
-        st = ST_100_101;
-        break;
-      #endif
-      */
-
-      lcd.print(F("Bem vindo!!"));
-      delay(250);
-      lcd.clear();
-      (read_config() ? (lcd.printf("SD OK!!: %d", sd_exist |= 0x01)) : (lcd.printf("Não há SD: %d", sd_exist &= ~0x01)));
-      delay(1000);
-      lcd.clear();  
-
-      st = MENU;
-
-      break;
-  
     case MENU:
-
-      if(old_pot != pot_sel)
-      {
-        lcd.setCursor(0,0);
-        lcd.print(F("MANGUE AVR - 4x4"));
-        lcd.setCursor(1,0);
-        lcd.print(F(" RUN "));
-        lcd.write('>');
-        old_pot = pot_sel;
-      }
-
-      if(B_SEL_CLICK)
-      {
-        delay(DEBOUCE_TIME);
-        st = RUN;
-
-        B_SEL_CLICK = false;
-      }
-      
-      /*
-      if(!digitalRead(B_SEL))
-      {
-        delay(DEBOUCE_TIME);
-        while(!digitalRead(B_SEL));
-        
-        st = pot_sel + RUN;
-        old_pot = -1;
-      }
-      */
-
+    {
       break;
-
-    case RUN:
-      // Send the message here
-      /* Not yet */
-      break;
-
-    case ST_30:
-      break;
-
-    case ST_100_101:
-      break;
+    }
   }
 }
 
@@ -220,11 +142,6 @@ void loop()
 void Pin_Config()
 {
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(B_SEL, INPUT_PULLUP);
-  pinMode(B_CANCEL, INPUT_PULLUP);
-
-  attachInterrupt(digitalPinToInterrupt(B_SEL), select_button, FALLING);
-  attachInterrupt(digitalPinToInterrupt(B_CANCEL), cancel_button, FALLING);
 
   return;
 }
@@ -233,62 +150,36 @@ void Pin_Config()
 void receiveCallBack(const uint8_t* macAddr, const uint8_t* data, int len)
 {
   // Called when data is received
-  char buffer[ESP_NOW_MAX_DATA_LEN + sizeof(char)];  
-  int msgLen = min(ESP_NOW_MAX_DATA_LEN, len);
-
-  strncpy(buffer, (const char *)data, msgLen);
-  buffer[msgLen] = 0; //Make sure we are null terminated
+  uint8_t recv=0;
   
-  // Format the MAC address
-  char macStr[18];
-  formatMacAddress(macAddr, macStr, 18);
+  memcpy(&recv, data, sizeof(data));
+  Serial.print("Data received: ");
+  Serial.println(len);
+  Serial.print("Message: ");
+  Serial.println(recv);
+  Serial.println();
 
-  Serial.printf("Receive message from: %s - %s\n", macStr, buffer);
-
-  if(!strcmp("ok", buffer))
+  if(recv==1)
   {
-
     #ifdef M_30
-      M_30_ok |= 0x01;
-      (sent_to_all("30_ok") ? M_30_ok << 1 : 0);
-
-      return;
-      
-    #elif M_100_101
-      M_100_101_ok |= 0x01;
-      (sent_to_all("100_101_ok") ? M_100_101_ok << 1 : 0);
-
-      return;
-
+      //confirm_30 |= recv;
+      //(sent_to_single(1) ? confirm_30 << recv : 0);
+      sent_to_single(0);
+    #elif defined(M_100_101)
+      //confirm_100 |= recv;
+      //(sent_to_single(2) ? confirm_100 << recv : 0);
+      sent_to_single(2);
     #endif
-
   }
 
-  if(!strcmp("30_ok", buffer))
-  {
-    listen_30 = true;
-    //lcd.print(F("Ouvindo 30"));
-    //delay(500);
-    //lcd.clear();
-  }
+  if(recv==0)
+    conf_30 = true;
 
-  if(!strcmp("100_101_ok", buffer))
-  {
-    listen_100_101 = true;
-    //lcd.print(F("Ouvindo 100 &"));
-    //lcd.setCursor(1,8); lcd.print(F("101"));
-    //delay(500);
-    //lcd.clear();
-  } 
+  if(recv==2)
+    conf_100 = true;
 }
 
-void formatMacAddress(const uint8_t* MACAddr, char* info, int maxLength)
-{
-  // Formats MAC Address
-  snprintf(info, maxLength, "%02x:%02x:%02x:%02x:%02x:%02x", MACAddr[0], MACAddr[1], MACAddr[2], MACAddr[3], MACAddr[4], MACAddr[5]);
-}
-
-void sentCallBack(const uint8_t* macAddr, esp_now_send_status_t status)
+void sentCallBack(const uint8_t* macAddr, esp_now_send_status_t status) 
 {
   // Called when data is sent
   char macStr[18];
@@ -302,92 +193,34 @@ void sentCallBack(const uint8_t* macAddr, esp_now_send_status_t status)
     esp_now_ok = true;
 }
 
-bool sent_to_all(const String &msg)
+void formatMacAddress(const uint8_t* MACAddr, char* info, int maxLength)
+{
+  // Formats MAC Address
+  snprintf(info, maxLength, "%02x:%02x:%02x:%02x:%02x:%02x", MACAddr[0], MACAddr[1], MACAddr[2], MACAddr[3], MACAddr[4], MACAddr[5]);
+}
+
+bool sent_to_all(const uint8_t msg)
 {
   // Broadcast a message to every device in range
-  uint8_t broadcastAdress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-  esp_now_peer_info_t peerInfo = {};
+  uint8_t BroadcastAdress[] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+  esp_now_peer_info_t BroadInfo = {};
 
-  memcpy(&peerInfo.peer_addr, broadcastAdress, 6);
+  memcpy(&BroadInfo.peer_addr, BroadcastAdress, 6);
 
-  if(!esp_now_is_peer_exist(broadcastAdress))
+  if(!esp_now_is_peer_exist(BroadcastAdress))
   {
-    esp_now_add_peer(&peerInfo);
+    esp_now_add_peer(&BroadInfo);
   }
 
   // Send message
-  esp_err_t result = esp_now_send(broadcastAdress, (const uint8_t *)msg.c_str(), msg.length());
+  esp_err_t result = esp_now_send(BroadcastAdress, (uint8_t *)&msg, sizeof(msg));
 
-  if(result==ESP_OK)
-  {
-    return true;
-  }
-
-  return false;
+  return result==ESP_OK ? true : false;
 }
 
-/* Global Functions */
-bool read_config()
+bool sent_to_single(const uint8_t msg)
 {
-  if(!SD.begin()) 
-    { delay(500); return false; }
-  else 
-    { Mount_SD(); return true; }
-}
+  esp_err_t result = esp_now_send(AddressFor_0, (uint8_t *)&msg, sizeof(msg));
 
-void Mount_SD()
-{
-  int countfiles_onSD = 0;
-  char file_name[20];
-  File root = SD.open("/");
-
-  while(true)
-  {
-    File entry = root.openNextFile();
-    if(!entry)
-      break; // no more files
-    
-    // for each file count it
-    countfiles_onSD++;
-    entry.close();
-  }
-    
-  sprintf(file_name, "/%s%d.csv", "AV_data", countfiles_onSD);
-
-  File data = SD.open(file_name, FILE_APPEND);
-
-  if(data)
-  {
-    //data.printf("VEI=%s,MOLA MOTORA=%s,PRE CARGA MOTORA=%s,PESO=%s,MOLA MOVIDA=%s,PRE CARGA MOVIDA=%s", 
-    //new_config.vei, new_config.mola_mot, new_config.pre_mot, new_config.peso_mot, new_config.mola_mov, new_config.pre_mov);
-    data.println("Diga qual é o");
-    data.println("Setup da CVT: ");
-    data.close();
-
-  } else {
-    Serial.println(F("FAIL TO OPEN THE FILE"));
-    sd_exist &= ~0x01;
-  }
-}
-
-char potSelect(uint8_t pin, uint8_t num_options)
-{
-  /* Mapeia o curso do potenciometro para percorrer 2 vezes as opções de seleção,
-  retorna o valor da opção escolhida */
-  int read_val = analogRead(pin);
-  uint8_t option = map(read_val, 50, 1000, 0, 2 * num_options - 1);
-  //    if (option >= num_options)
-  //        option -= num_options;
-  return option % num_options;
-}
-
-/* Interrupts routine */
-void select_button()
-{
-  B_SEL_CLICK = true;
-}
-
-void cancel_button()
-{
-  B_CANCEL_CLICK = true;
+  return result==ESP_OK ? true : false;
 }

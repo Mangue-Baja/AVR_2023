72ca6f257974ee476a05e7f56f3900ef5ff7d3f5
joaogabrieldonato <jgsd.2001@gmail.com>
2023-11-09 12:11:16 -0300

    Add time variables

--- include/soft_defs.h
+++ include/soft_defs.h
@@ -5,8 +5,24 @@
 
 #define DEBOUCE_TIME 150 
 
-typedef enum {INIT, WAIT, MENU} state; 
+typedef enum {INIT, WAIT, SD_BEGIN, MENU, RUN} state; 
+typedef enum {START_} run_state;
 
 uint8_t st = INIT;
+uint8_t ss_r = START_;
+
+unsigned long int t_curr = 0;
+volatile unsigned long int t_30 = 0, t_100 = 0, t_101 = 0;
+float vel = 0;
+
+String str_30 = "00:000",
+       str_100 = "00:000",
+       str_101 = "00:000",
+       str_vel = "00.00 km/h";
+
+bool esp_now_ok = false;
+bool conf_30 = false, conf_100 = false;
+
+int8_t pot_sel = 0, old_pot = -1;
 
 #endif      

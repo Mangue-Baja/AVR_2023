bab2424c8f1b907eeeacbd19b50e38b71f05a0ce
joaogabrieldonato <jgsd.2001@gmail.com>
2023-11-13 21:59:44 -0300

    Add run state

--- include/soft_defs.h
+++ include/soft_defs.h
@@ -6,14 +6,21 @@
 #define DEBOUCE_TIME 150 
 
 typedef enum {INIT, WAIT, SD_BEGIN, MENU, RUN} state; 
-typedef enum {START_} run_state;
+typedef enum {START_, LCD_DISPLAY, WAIT_30, WAIT_100, END_RUN, SAVE_RUN} run_state;
 
-uint8_t st = INIT;
+uint8_t ss_t = INIT;
 uint8_t ss_r = START_;
 
 unsigned long int t_curr = 0;
 volatile unsigned long int t_30 = 0, t_100 = 0, t_101 = 0;
 float vel = 0;
+
+typedef struct {
+    uint8_t flag = 0x00;
+    volatile unsigned long int tt_30 = 0, tt_100 = 0, tt_101 = 0;
+} packet_t;
+
+packet_t packet;
 
 String str_30 = "00:000",
        str_100 = "00:000",
@@ -23,6 +30,7 @@ String str_30 = "00:000",
 bool esp_now_ok = false;
 bool conf_30 = false, conf_100 = false;
 
+uint8_t pos[2];
 int8_t pot_sel = 0, old_pot = -1;
 
 #endif      

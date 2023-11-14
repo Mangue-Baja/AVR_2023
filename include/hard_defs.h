fab49762e900c723442e13bb372a7ac41260e5e8
joaogabrieldonato <jgsd.2001@gmail.com>
2023-11-13 21:59:27 -0300

    Add the pinout of the PCB

--- include/hard_defs.h
+++ include/hard_defs.h
@@ -1,10 +1,24 @@
 #ifndef HARD_DEFS
 #define HARD_DEFS
 
-#define SD_CS 2 // CS Pin
+/* Defines SD pins */
+#define SD_CS 2
+#define SCLK  18
+#define MISO  19
+#define MOSI  23
 
-/* Defines buttons */
-#define B_SEL 15
-#define B_CAN 4
+/* Defines LCD pins */
+#define SCL   22
+#define SDA   21
+
+/* Defines buttons and potenciometer */
+#define B_SEL 16
+#define B_CAN 17
+#define POT   26
+
+/* Defines Sensors pins */
+#define SENSOR_ZERO   12
+#define SENSOR_30_100 15
+#define SENSOR_101    13
 
 #endif

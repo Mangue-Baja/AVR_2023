7a0c1d4415eb6c92d5fc12880890b67588fddafb
joaogabrieldonato <jgsd.2001@gmail.com>
2023-11-16 14:09:10 -0300

    Remove useless struct

--- include/soft_defs.h
+++ include/soft_defs.h
@@ -11,22 +11,16 @@ typedef enum {START_, LCD_DISPLAY, WAIT_30, WAIT_100, END_RUN, SAVE_RUN} run_sta
 uint8_t ss_t = INIT;
 uint8_t ss_r = START_;
 
+volatile unsigned long int time_101 = 0;
 unsigned long int t_curr = 0;
 volatile unsigned long int t_30 = 0, t_100 = 0, t_101 = 0;
 float vel = 0;
-
-typedef struct {
-    uint8_t flag = 0x00;
-    volatile unsigned long int tt_30 = 0, tt_100 = 0, tt_101 = 0;
-} packet_t;
-
-packet_t packet;
-
 String str_30 = "00:000",
        str_100 = "00:000",
        str_101 = "00:000",
        str_vel = "00.00 km/h";
 
+uint8_t flag = 0x00;
 bool esp_now_ok = false;
 bool conf_30 = false, conf_100 = false;
 

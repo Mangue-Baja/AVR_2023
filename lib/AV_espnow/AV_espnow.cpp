75657e48d9c1880009637e6f360ed4b8f37bd76f
joaogabrielsantosD <joao.donato@ufpe.br>
2024-07-03 12:35:50 -0300

    FEAT: Register peer

--- lib/AV_espnow/AV_espnow.cpp
+++ lib/AV_espnow/AV_espnow.cpp
@@ -1,13 +1,31 @@
 #include "AV_espnow.h"
 
+esp_now_peer_info_t peerInfo;
+
 bool init_esp_now()
 {
     return esp_now_init() == ESP_OK ? true : false;
 }
 
-void register_receive_callback(esp_now_callback receive_callback)
+bool register_peer(uint8_t* mac)
 {
-    esp_now_register_recv_cb(receive_callback);
+    // Register peer
+    memcpy(peerInfo.peer_addr, mac, 6);
+    peerInfo.channel = 0;
+    peerInfo.encrypt = false;
+
+    // add peer
+    if (esp_now_add_peer(&peerInfo) != ESP_OK)
+    {
+        Serial.println("Failed to add peer");
+        return false;
+    }
+    return true;
+}
+
+bool register_receive_callback(esp_now_callback receive_callback)
+{
+    return esp_now_register_recv_cb(receive_callback) == ESP_OK ? true : false;
 }
 
 bool sent_to_all(void *AnyMessage, int size)
@@ -18,11 +36,18 @@ bool sent_to_all(void *AnyMessage, int size)
 
     memcpy(&BroadInfo.peer_addr, BroadcastAdress, 6);
 
-    if(!esp_now_is_peer_exist(BroadcastAdress)) 
+    if (!esp_now_is_peer_exist(BroadcastAdress))
         esp_now_add_peer(&BroadInfo);
 
     // Send message
     esp_err_t result = esp_now_send(BroadcastAdress, (uint8_t *)AnyMessage, size);
 
-    return result==ESP_OK ? true : false;
+    return result == ESP_OK ? true : false;
+}
+
+bool sent_to_single(void *AnyMessage, int size, uint8_t* address_from_receive)
+{
+    esp_err_t result = esp_now_send(address_from_receive, (uint8_t *)AnyMessage, size);
+
+    return result == ESP_OK ? true : false;
 }
--- lib/AV_espnow/AV_espnow.h
+++ lib/AV_espnow/AV_espnow.h
@@ -4,11 +4,13 @@
 #include <Arduino.h>
 #include <esp_now.h>
 
-typedef void (*esp_now_callback)(const uint8_t*, const uint8_t*, int);
+typedef void (*esp_now_callback)(const uint8_t *, const uint8_t *, int);
 
 bool init_esp_now();
-void register_receive_callback(esp_now_callback receive_callback);
+bool register_peer(uint8_t* mac);
+bool register_receive_callback(esp_now_callback receive_callback);
 
 bool sent_to_all(void *AnyMessage, int size);
+bool sent_to_single(void *AnyMessage, int size, uint8_t* address_from_receive);
 
 #endif

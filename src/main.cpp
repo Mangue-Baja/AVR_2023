9b7d174cd9bb2b689642e6a64cde5e1f7eda9cd3
joaogabrielsantosD <joao.donato@ufpe.br>
2024-07-04 11:09:03 -0300

    Comentarios sobre as maquinas de estado

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,24 @@
 #include <Arduino.h>
 #include <Modules.h>
 
-#define Module_0m   0x00 // id do modulo
-#define Module_30m  0x03 // id do modulo
-#define Module_100m 0x10 // id do modulo
-#define BRIDGE      0xff // id do modulo
+#define Module_0m   0x00 // id do modulo inicial (0 metros)
+#define Module_30m  0x03 // id do modulo de 30 metros
+#define Module_100m 0x10 // id do modulo de 100 metros
+#define BRIDGE      0xff // id do modulo usado como ponte
 
-/* Selecione por meio desse define para qual modulo esta indo o Firmware
+/* Selecione por meio desse define para qual modulo esta indo o Firmware:
  * Module_0m == 0x00, referente ao modulo fixado no inicio do AV(0 metros) que
  possui com ele o modulo LCD para mostrar o tempo e a velocidade do prototipo
  juntamente com o modulo SD para armazenar esses dados;
 
- * Module_30m == 0x03, modulo com sensor fixado nos 30 metros para aquisição da aceleração;
+ * Module_30m == 0x03, modulo com sensor fixado nos 30 metros para aquisição da 
+  aceleração (tempo em 30 metros);
 
  * Module_100m == 0x10, modulo com sensor fixado nos 100 e 101 metros para aquisição da velocidade
  e também do tempo em 100 metros;
 
- * BRIDGE == 0xff, apenas um modulo que servira de ponte para que a comunicação ESP-NOW possa acontecer
- sem que haja erro de sinal ou até mesmo não conseguir se comunicar.
+ * BRIDGE == 0xff, apenas um modulo secundario que servira de ponte para que a comunicação ESP-NOW possa acontecer
+ sem que haja erro de sinal ou de conectividade com os modulos principais.
 */
 #define MODE Module_0m
 
